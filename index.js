528f739edb3762ae3b8f3b9ff27f67720ed7b45e
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-05-05 08:40:38 -0400

    fixed let to const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const num1 = 31;
 const num2 = 2;
 const multiply = num1 * num2;
 
-let random = Math.floor(Math.random() * 40) + 6;
+const random = Math.floor(Math.random() * 40) + 6;
 
 const num3 = 10;
 const num4 = 6;
